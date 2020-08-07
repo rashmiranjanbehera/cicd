ddc12d39ee0efc3c0924d337e2643927431348f8
Rashmiranjan Behera <rashmiranjan.b@indianeagle.com>
2020-08-07 06:25:26 -0500

    Remote docker configuration

--- src/main/java/testng/CicdSample.java
+++ src/main/java/testng/CicdSample.java
@@ -1,8 +1,13 @@
 package testng;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,11 +19,17 @@ public class CicdSample
 
     WebDriver obj;
     public void launch(String url) {
-        String driverPath = "src/main/java/driver/geckodriver.exe";
-        System.setProperty("webdriver.gecko.driver", driverPath);
-        obj = new FirefoxDriver();
+        WebDriverManager.firefoxdriver().setup();
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setBrowserName("firefox");
+        try {
+            obj = new RemoteWebDriver(new URL("http://ec2-100-27-29-34.compute-1.amazonaws.com:4444/wd/hub"),cap);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         obj.get("https://" + url);
         obj.manage().timeouts().implicitlyWait(50000, TimeUnit.MILLISECONDS);
+        System.out.println("#####################................."+obj.findElement(By.id("homePageId")).getText());
         windowMaximize(obj);
         obj.quit();
     }
